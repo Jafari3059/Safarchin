e854e5990b62c194fcf6f21633f850f4e71c3afc
fereshte <fereshtejafari3059@gmail.com>
2025-05-12 13:48:45 +0330

    add content to checklist and notes fildes

--- app/src/main/java/com/example/safarchin/ui/theme/FourPageAsli/Plannig/overviewP/OverviewTabContent.kt
+++ app/src/main/java/com/example/safarchin/ui/theme/FourPageAsli/Plannig/overviewP/OverviewTabContent.kt
@@ -3,6 +3,7 @@ package com.example.safarchin.ui.theme.FourPageAsli.Plannig.overviewP
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -10,11 +11,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -47,6 +53,12 @@ fun OverviewTabContent(
     var checklistExpanded by remember { mutableStateOf(false) }
     var locationsExpanded by remember { mutableStateOf(false) }
 
+    val checklistInput = remember { mutableStateOf("") }
+    val checklistItems = remember { mutableStateListOf<Pair<String, Boolean>>() }
+
+    val noteInput = remember { mutableStateOf("") }
+    val noteItems = remember { mutableStateListOf<String>() }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -66,12 +78,124 @@ fun OverviewTabContent(
             isExpanded = notesExpanded,
             onToggle = { notesExpanded = !notesExpanded }
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(inputHeight * 3)
-            ) {}
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(6.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFFF6F4F4), RoundedCornerShape(16.dp))
+                        .padding(horizontal = 10.dp, vertical = 8.dp)
+                ) {
+                    TextField(
+                        value = noteInput.value,
+                        onValueChange = { noteInput.value = it },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(70.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent
+                        ),
+                        shape = RoundedCornerShape(12.dp),
+                        textStyle = TextStyle(
+                            textAlign = TextAlign.Right,
+                            fontFamily = iranSans
+                        ),
+                        placeholder = {
+                            Text(
+                                "...چیزی بنویسید",
+                                fontFamily = iranSans,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 12.sp,
+                                color = Color.Gray,
+                                textAlign = TextAlign.Right,
+                                modifier = Modifier.fillMaxWidth().padding(end = 0.dp)
+                            )
+                        },
+                        singleLine = false,
+                        maxLines = 4
+                    )
+
+                    Button(
+                        onClick = {
+                            val input = noteInput.value.trim()
+                            if (input.isNotEmpty()) {
+                                noteItems.add(input)
+                                noteInput.value = ""
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFB26B)),
+                        shape = RoundedCornerShape(10.dp),
+                        contentPadding = PaddingValues(horizontal = 12.dp, vertical = 6.dp),
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                           // .padding(start = 0.dp, bottom = 0.dp)
+                            .offset(x = 2.dp, y=4.dp)
+                            .width(66.dp)
+                    ) {
+                        Text("ثبت", fontFamily = iranSans, color = Color.White)
+                    }
+                }
+
+                noteItems.forEachIndexed { index, text ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(IntrinsicSize.Min)
+                            .padding(1.dp)
+                    ) {
+                        Canvas(modifier = Modifier.matchParentSize()) {
+                            drawRoundRect(
+                                color = Color.Black,
+                                size = size,
+                                cornerRadius = CornerRadius(16.dp.toPx(), 16.dp.toPx()),
+                                style = Stroke(
+                                    width = 2.5f,
+                                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 8f))
+                                )
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color(0xFFF6F4F4), RoundedCornerShape(16.dp))
+                                .padding(horizontal = 12.dp, vertical = 10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.trashcan),
+                                contentDescription = null,
+                                tint = Color(0xFF2D2D2D),
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .clickable {
+                                        noteItems.removeAt(index)
+                                    }
+                            )
+
+                            Text(
+                                text = text,
+                                fontFamily = iranSans,
+                                fontSize = 14.sp,
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(end = 8.dp),
+                                textAlign = TextAlign.Right,
+                                maxLines = 5,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
+                    }
+                }
+            }
         }
+
         Spacer(modifier = Modifier.height(spacerHeight))
 
         OverviewInputField(
@@ -81,12 +205,133 @@ fun OverviewTabContent(
             isExpanded = checklistExpanded,
             onToggle = { checklistExpanded = !checklistExpanded }
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(inputHeight * 3)
-            ) {}
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(6.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFFF6F4F4), RoundedCornerShape(16.dp))
+                        .padding(horizontal = 10.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            val input = checklistInput.value.trim()
+                            if (input.isNotEmpty()) {
+                                checklistItems.add(input to false)
+                                checklistInput.value = ""
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFB26B)),
+                        shape = RoundedCornerShape(10.dp),
+                        contentPadding = PaddingValues(horizontal = 12.dp, vertical = 6.dp),
+                        modifier = Modifier.width(66.dp)
+                    ) {
+                        Text("ثبت", fontFamily = iranSans, color = Color.White)
+                    }
+
+                    Spacer(modifier = Modifier.width(6.dp))
+
+                    TextField(
+                        value = checklistInput.value,
+                        onValueChange = { checklistInput.value = it },
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(48.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent
+                        ),
+                        shape = RoundedCornerShape(12.dp),
+                        textStyle = TextStyle(
+                            textAlign = TextAlign.Right,
+                            fontFamily = iranSans
+                        ),
+                        placeholder = {
+                            Text(
+                                "...آیتم جدید",
+                                fontFamily = iranSans,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 12.sp,
+                                color = Color.Gray,
+                                textAlign = TextAlign.Right,
+                                modifier = Modifier.fillMaxWidth().padding(end = 0.dp)
+                            )
+                        },
+                        singleLine = true,
+                        maxLines = 1
+                    )
+                }
+
+                checklistItems.forEachIndexed { index, (text, isChecked) ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(IntrinsicSize.Min)
+                            .padding(1.dp)
+                    ) {
+                        Canvas(modifier = Modifier.matchParentSize()) {
+                            drawRoundRect(
+                                color = Color.Black,
+                                size = size,
+                                cornerRadius = CornerRadius(16.dp.toPx(), 16.dp.toPx()),
+                                style = Stroke(
+                                    width = 2.5f,
+                                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 8f))
+                                )
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color(0xFFF6F4F4), RoundedCornerShape(16.dp))
+                                .padding(horizontal = 12.dp, vertical = 10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.trashcan),
+                                contentDescription = null,
+                                tint = Color(0xFF2D2D2D),
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .clickable {
+                                        checklistItems.removeAt(index)
+                                    }
+                            )
+
+                            Text(
+                                text = text,
+                                fontFamily = iranSans,
+                                fontSize = 12.sp,
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(end = 8.dp),
+                                textAlign = TextAlign.Right,
+                                maxLines = 5,
+                                overflow = TextOverflow.Ellipsis
+                            )
+
+                            Box(
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .clip(CircleShape)
+                                    .background(if (isChecked) Color(0xFF939B62) else Color.LightGray)
+                                    .clickable {
+                                        checklistItems[index] = checklistItems[index].copy(second = !isChecked)
+                                    }
+                            ) {}
+                        }
+                    }
+                }
+            }
         }
+
         Spacer(modifier = Modifier.height(spacerHeight))
 
         OverviewInputField(
@@ -102,8 +347,7 @@ fun OverviewTabContent(
                         .fillMaxWidth()
                         .height(inputHeight * 3)
                 ) {}
-            }
-            else {
+            } else {
                 Column(
                     verticalArrangement = Arrangement.spacedBy(2.dp),
                     modifier = Modifier
@@ -113,7 +357,7 @@ fun OverviewTabContent(
                     selectedPlacesViewModel.selectedPlaces.forEach { place ->
                         val tourPlace = TourPlace(
                             name = place.name,
-                            description = "میدان نقش جهان یا میدان امام اصفهان، یکی از مهم ترین جاذبه های گردشگری و میدان مرکزی شهراصفهان است. در این میدان بسیاری دیگر از بناهای تاریخی و باستانی نیز قرار گرفته است. \u2028میدان نقش جهان اصفهان، ثبت سازمان یونسکو شده است به همین دلیل شهرت و شکوه بین المللی دارد همچنین میدان نقش جهان گنجینه شهر اصفهان است که سالانه  ",
+                            description = "میدان نقش جهان یا میدان امام اصفهان، یکی از مهم ترین جاذبه های گردشگری و میدان مرکزی شهراصفهان است...",
                             imageRes = place.imageRes,
                             Visit_duration = "۱ ساعت",
                             Visit_price = "۲۰٬۰۰۰ تومان",
@@ -121,26 +365,16 @@ fun OverviewTabContent(
                             telephone = 12345678,
                             WorkingHours = "۹ تا ۱۷"
                         )
-                            Box(
-                                modifier = Modifier
-                                   // .fillMaxWidth()
-                                   // .padding(2.dp) // فاصله از اطراف برای نمایش سایه
-                                   // .shadow(6.dp, RoundedCornerShape(2.dp))
-                                   // .clip(RoundedCornerShape(8.dp))
-                                   // .background(Color(0x40000000),)
-                            )
-                        {
-                            TourPlaceCard(
-                                place = tourPlace,
-                                onRemoveClick = {
-                                    selectedPlacesViewModel.removePlace(place.id)
-                                    if (selectedPlacesViewModel.selectedPlaces.isEmpty()) {
-                                        locationsExpanded = false
-                                    }
-                                },
-                                showRemove = true
-                            )
-                        }
+                        TourPlaceCard(
+                            place = tourPlace,
+                            onRemoveClick = {
+                                selectedPlacesViewModel.removePlace(place.id)
+                                if (selectedPlacesViewModel.selectedPlaces.isEmpty()) {
+                                    locationsExpanded = false
+                                }
+                            },
+                            showRemove = true
+                        )
                     }
                 }
             }
@@ -177,7 +411,6 @@ fun OverviewTabContent(
     }
 }
 
-
 @Composable
 fun OverviewInputField(
     placeholder: String,
@@ -185,7 +418,7 @@ fun OverviewInputField(
     onToggle: () -> Unit,
     height: Dp = 50.dp,
     width: Dp = 348.dp,
-    content: @Composable (() -> Unit)? = null // ✅ اضافه شده
+    content: @Composable (() -> Unit)? = null
 ) {
     val screenWidth = LocalConfiguration.current.screenWidthDp
     val fontSize = (screenWidth * 0.03).sp
@@ -227,7 +460,7 @@ fun OverviewInputField(
                 )
             }
 
-            // ✅ محتوا وقتی فیلد باز بشه
+            //  محتوا وقتی فیلد باز بشه
             if (isExpanded && content != null) {
                 Spacer(modifier = Modifier.height(8.dp))
                 content()
@@ -266,20 +499,3 @@ fun PreviewOverviewTabContent() {
     }
 }
 
-//@Preview(showBackground = true, locale = "fa")
-//@Composable
-//fun PreviewOverviewTabContent() {
-//    val fakeViewModel = SavedPlacesViewModel().apply {
-//        addPlace(SavedPlace(1, "مسجد نصیرالملک", R.drawable.shiraz))
-//    }
-//
-//    MaterialTheme {
-//        Surface(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .background(Color(0xFFF6F4F4))
-//        ) {
-//            OverviewTabContent(savedPlacesViewModel = fakeViewModel)
-//        }
-//    }
-//}
